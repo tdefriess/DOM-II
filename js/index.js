b450bff2daf7495d525a5321eee3e67de5d91c02
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-14 14:26:29 -0500

    Added two event listeners: mouseover and mouseleave to links

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 // Your code goes here
 
+document.querySelectorAll('a').forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.transition = '1s';
+        element.style.backgroundColor = 'papayawhip';
+    })
+    element.addEventListener('mouseleave', () => {
+        element.style.backgroundColor = 'white';
+    })
+})
+
